38c498addb0363e401762ac01da03086a97b7c59
mmn <muhammadmnorouzi@gmail.com>
2021-04-23 17:51:21 +0430

    End of section 9

---
+++ ClientAngular/src/app/_models/Member.ts
@@ -0,0 +1,18 @@
+import { Photo } from "./Photo";
+
+export interface Member {
+    id: number;
+    username: string;
+    age: number;
+    photoUrl: string;
+    knownAs: string;
+    created: Date;
+    lastActive: Date;
+    gender: string;
+    introduction: string;
+    lookingFor: string;
+    intrests?: any;
+    city: string;
+    country: string;
+    photos: Photo[];
+}
